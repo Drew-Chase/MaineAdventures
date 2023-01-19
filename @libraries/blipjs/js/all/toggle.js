73f0bfb1fcd9a03992a3cf0ba566479b882dbc5c
Drew Chase <dcmanproductions@gmail.com>
2023-01-18 23:02:14 -0500

    Udated Site

--- @libraries/blipjs/js/all/toggle.js
+++
@@ -1,9 +0,0 @@
-$(document).on('nav-complete', () => {
-    Array.from($("toggle")).forEach(toggle => {
-        $(toggle).attr('value', $(toggle).attr('value') == null ? "false" : $(toggle).attr('value'))
-        $(toggle).on('click', () => {
-            $(toggle).attr('value', $(toggle).attr('value') == "false")
-            toggle.dispatchEvent(new ToggleEvent($(toggle).attr('value') == "true"));
-        })
-    })
-}) 
