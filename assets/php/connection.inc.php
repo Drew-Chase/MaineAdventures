d2e493fd4b388f59044fad5c3dfcf339d30a7d16
Drew Chase <dcmanproductions@gmail.com>
2023-02-02 19:35:37 -0500

    added script to handle sql connections

---
+++ assets/php/connection.inc.php
@@ -0,0 +1,22 @@
+<?php
+require "values.inc.php";
+class Connection
+{
+    var $conn;
+    function __construct()
+    {
+        global $servername;
+        global $username;
+        global $password;
+        global $db;
+        $this->conn = new mysqli($servername, $username, $password, $db);
+        if ($this->conn->connect_error) {
+            http_response_code(500);
+            die(json_encode(array("error" => $this->conn->error)));
+        }
+    }
+    function __destruct()
+    {
+        $this->conn->close();
+    }
+}
