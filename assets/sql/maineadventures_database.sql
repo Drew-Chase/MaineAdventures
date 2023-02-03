ece84e0c7d7308f9e0bcbe245df554b49fbfdf1d
Drew Chase <dcmanproductions@gmail.com>
2023-02-02 19:39:35 -0500

    added sql file for database creation

---
+++ assets/sql/maineadventures_database.sql
@@ -0,0 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 03, 2023 at 01:38 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maineadventures`
+--
+CREATE DATABASE IF NOT EXISTS `maineadventures` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `maineadventures`;
