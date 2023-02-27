d2ce0847246b6bd18775a5b70ce762c0f20212b1
Drew Chase <dcmanproductions@gmail.com>
2023-02-27 14:17:25 -0500

    added comments

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,6 +1,12 @@
+/**
+ * It calls the phone number listed.
+ */
 function Call() {
     window.location.href = `tel:+1 (207) 723-9215`
 }
+/**
+ * It opens the default email client and creates a new email with the subject line "Contact US"
+ */
 function Email() {
     window.location.href = `mailto:contact-us@maineadventures.org?subject=Contact US`
 }
