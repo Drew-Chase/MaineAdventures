2329d8205d0676f08eac402cdd61bad1ce9e8899
Drew Chase <dcmanproductions@gmail.com>
2023-02-27 14:17:58 -0500

    added cabin route

---
+++ assets/php/cabins.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+require "connection.inc.php";
+$cabins = new Cabins();
+if (isset($_GET["c"])) {
+    switch ($_GET["c"]) {
+        case "get":
+            $cabins->GetCabins();
+            break;
+        default:
+            http_response_code(500);
+            die(json_encode(["error" => "No such category was found: " . $_GET["c"]]));
+            break;
+    }
+} else {
+    http_response_code(404);
+    die(json_encode(["error" => "No category was specified!"]));
+}
+
+class Cabins
+{
+    var $connection;
+    function __construct()
+    {
+        $this->connection = new Connection();
+    }
+
+    function GetCabins()
+    {
+        $conn = $this->connection->conn;
+        $sql = "SELECT * FROM `cabins`";
+        $query = $conn->query($sql);
+        if ($query && $query->num_rows > 0) {
+            $cabins = array();
+            while ($result = $query->fetch_assoc()) {
+                array_push($cabins, $result);
+            }
+            http_response_code(200);
+            die(json_encode($cabins));
+        }
+    }
+}
