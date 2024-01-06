d952c8421c09e6b31478a15d9c83b8fa6a931d21
Drew Chase <dcmanproductions@gmail.com>
2024-01-06 12:00:49 -0500

    Refactor gallery.js to improve code structure and readability

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,36 +1,38 @@
-var selectedGalleryImage = null;
-$(".gallery-image img").each((_, i) => {
-    if (i.complete) {
-        load(i.parentElement)
-    }
-})
+(() => {
+    var selectedGalleryImage = null;
+    $(".gallery-image img").each((_, i) => {
+        if (i.complete) {
+            load(i.parentElement)
+        }
+    })
 
-$(".gallery-image img").on('load', e => load(e.currentTarget.parentElement))
-$(".gallery-image:not(.admin)").on('click', e => open(e.currentTarget))
-$("body").on('click', e => {
-    if (e.target.tagName == "BODY") {
-        if (selectedGalleryImage != null) {
-            close();
+    $(".gallery-image img").on('load', e => load(e.currentTarget.parentElement))
+    $(".gallery-image:not(.admin)").on('click', e => open(e.currentTarget))
+    $("body").on('click', e => {
+        if (e.target.tagName == "BODY") {
+            if (selectedGalleryImage != null) {
+                close();
+            }
+        }
+    })
+
+    function load(gallery) {
+        gallery = $(gallery)
+        gallery.addClass('loaded')
+    }
+    function open(gallery) {
+        if (selectedGalleryImage == null) {
+            gallery = $(gallery)
+            gallery.addClass('fullscreen')
+            $("body").addClass('overlay')
+            selectedGalleryImage = gallery;
         }
     }
-})
-
-function load(gallery) {
-    gallery = $(gallery)
-    gallery.addClass('loaded')
-}
-function open(gallery) {
-    if (selectedGalleryImage == null) {
-        gallery = $(gallery)
-        gallery.addClass('fullscreen')
-        $("body").addClass('overlay')
-        selectedGalleryImage = gallery;
+    function close() {
+        if (selectedGalleryImage != null) {
+            selectedGalleryImage.removeClass('fullscreen')
+            $('body').removeClass('overlay')
+            selectedGalleryImage = null;
+        }
     }
-}
-function close() {
-    if (selectedGalleryImage != null) {
-        selectedGalleryImage.removeClass('fullscreen')
-        $('body').removeClass('overlay')
-        selectedGalleryImage = null;
-    }
-}
+})()
