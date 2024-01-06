fbf0d5af4e81f39c5ab59a04ea84f2eb43bc3b08
Drew Chase <dcmanproductions@gmail.com>
2024-01-06 12:00:44 -0500

    Update contact-us.js with new functionality

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -2,11 +2,18 @@
  * It calls the phone number listed.
  */
 function Call() {
-    window.location.href = `tel:+1 (207) 723-9215`
+    // window.location.href = `tel:+1 (207) 723-9215`
+    window.open(`tel:+1 (207) 723-9215`)
 }
 /**
  * It opens the default email client and creates a new email with the subject line "Contact US"
  */
 function Email() {
-    window.location.href = `mailto:contact-us@maineadventures.org?subject=Contact US`
+    window.open(`mailto:bscanlin1@yahoo.com?subject=Contact US`)
+}
+function Book() {
+    let arrivalDate = new Date()
+    let departureDate = new Date();
+    departureDate.setDate(departureDate.getDate() + 7)
+    window.open(`mailto:bscanlin1@yahoo.com?subject=Book&body=Name:%0D%0AEmail:%0D%0APhone:%0D%0AArrival Date:${arrivalDate.toDateString()}%0D%0ADeparture Date:${departureDate.toDateString()}%0D%0AAdults:%0D%0AChildren:%0D%0A`)
 }
